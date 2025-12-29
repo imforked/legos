3165995e223b17d5aa5b5b0093342a555c48f077
imforked <10011101.dev@gmail.com>
2025-12-29 12:38:11 -0800

    Export Modal from library.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
-export { Button } from "./Button/Button";
-export { Cascade } from "./Cascade/Cascade";
-export { Input } from "./Input/Input";
-export { SignUp } from "./SignUp/SignUp";
+export { Button } from './Button/Button';
+export { Cascade } from './Cascade/Cascade';
+export { Input } from './Input/Input';
+export { SignUp } from './SignUp/SignUp';
+export { Modal } from './Modal/Modal';
